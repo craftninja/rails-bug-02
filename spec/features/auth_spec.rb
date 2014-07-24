ed914ef945bbf53efe8382ddf2f5adc16aa588a8
EmilyPlatzer <emilyplatzer@gmail.com>
2014-07-24 09:22:17 -0600

    User is logged in upon registration

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,15 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'User is logged in upon registering for site' do
+    visit root_path
+    click_on 'Register'
+    fill_in 'Name', :with => 'User'
+    fill_in 'Email', :with => 'user@example.com'
+    fill_in 'Password', :with => 'password'
+    fill_in 'Confirm', :with => 'password'
+    click_button 'Register'
+    expect(page).to have_content('You are logged in successfully.')
+  end
+
 end
